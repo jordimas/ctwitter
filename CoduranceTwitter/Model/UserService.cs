81001e2cd82d728a2f7a627349a089401326f319
Jordi Mas <jmas@softcatala.org>
2016-05-12 06:15:16 +0200

    Rename

---
+++ CoduranceTwitter/Model/UserService.cs
@@ -0,0 +1,31 @@
+﻿using CoduranceTwitter.DAL;
+
+namespace CoduranceTwitter.Model
+{
+    public class UserService
+    {   
+        private IRepository _repository;
+
+        public UserService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public User GetOrCreateUser(string username)
+        {
+            int? id = 0;
+            id = _repository.GetUser(username);
+
+            if (id.HasValue == false)
+            {
+                id = _repository.CreateUser(username);
+            }
+
+            return new User()
+            {
+                Id = id,
+                Username = username
+            };
+        }
+    }
+}
