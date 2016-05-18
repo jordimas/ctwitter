de92dc0d44d562474ac587a298e281bbe7fc1691
Jordi Mas <jmas@softcatala.org>
2016-05-18 20:48:46 +0200

    Fix UT

--- CoduranceTwitter.Tests/Model/Messages/CommandPostTests.cs
+++ CoduranceTwitter.Tests/Model/Messages/CommandPostTests.cs
@@ -31,11 +31,11 @@ namespace CoduranceTwitter.Tests.Model.Messages
         {
             var memoryMessageRepository = new MemoryMessageRepository();
             var memoryUserRepository = new MemoryUserRepository();
-            string text = "Alice -> Hello Bob";
+            string text = $"{TEST_USER1} -> {TEST_TEXT1}";
             var commandPost = new CommandPost(memoryMessageRepository, memoryUserRepository);
             commandPost.Process(text);
 
-            var user = memoryUserRepository.Get("Alice");
+            var user = memoryUserRepository.Get(TEST_USER1);
             Assert.IsNotNull(user);
         }
     }
