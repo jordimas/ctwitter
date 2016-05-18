aa859962b082c5903259f6a2272d99ade382e3bd
Jordi Mas <jmas@softcatala.org>
2016-05-18 14:02:05 +0200

    More UT

--- CoduranceTwitter.Tests/Model/CommandReadTests.cs
+++ CoduranceTwitter.Tests/Model/CommandReadTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoduranceTwitter.DAL;
 using CoduranceTwitter.Model;
 
@@ -7,88 +8,63 @@ namespace CoduranceTwitter.Tests.Model
     [TestClass]
     public class CommandReadTests : BaseTest
     {
-
         readonly string TEST_USER1 = "test-user1";
-        //readonly string TEST_USER2 = "test-user2";
         readonly string TEST_TEXT1 = "test-text1";
-        //readonly string TEST_TEXT2 = "test-text2";
-        
+        readonly string TEST_TEXT2 = "test-text2";
+
         [TestMethod]
-        public void PostMessage_OneMessage()
+        public void CommandRead_NoMessage()
         {
             var memoryMessageRepository = new MemoryMessageRepository();
             var memoryUserRepository = new MemoryUserRepository();
             var commandRead = new CommandRead(memoryMessageRepository, memoryUserRepository);
 
             var user = new User(TEST_USER1);
-            var message = new Message()
-            {
-                Text = TEST_TEXT1,
-                Username = user,
-            };
+            memoryUserRepository.Add(user);
+
+            commandRead.Process(TEST_USER1);
+            var messages = commandRead.Output;
+            Assert.AreEqual(0, messages.Count);
+        }
+
+        [TestMethod]
+        public void CommandRead_OneMessage()
+        {
+            var memoryMessageRepository = new MemoryMessageRepository();
+            var memoryUserRepository = new MemoryUserRepository();
+            var commandRead = new CommandRead(memoryMessageRepository, memoryUserRepository);
+
+            var user = new User(TEST_USER1);
+            var message = new Message() { Text = TEST_TEXT1, Username = user };
             memoryUserRepository.Add(user);
             memoryMessageRepository.Add(message);
 
             commandRead.Process(TEST_USER1);
-            var output = commandRead.Output;
-            Assert.AreEqual(1, output.Count);
-            Assert.AreEqual(TEST_USER1, output[0].Username.Username);
+            var messages = commandRead.Output;
+            Assert.AreEqual(1, messages.Count);
+            Assert.AreEqual(TEST_USER1, messages[0].Username.Username);
         }
-
         
-        /*[TestMethod]
-        public void PostMessage_CreatesUser()
+        [TestMethod]
+        public void CommandRead_Sorted()
         {
             var memoryMessageRepository = new MemoryMessageRepository();
             var memoryUserRepository = new MemoryUserRepository();
-            MessageService message = new MessageService(repository, userRepository);
-            message.PostMessage(TEST_USER1, TEST_TEXT1);
+            var commandRead = new CommandRead(memoryMessageRepository, memoryUserRepository);
 
-            var user = userRepository.Get(TEST_USER1);
-            Assert.AreEqual(user.Username, TEST_USER1);
-        }*/
-
-        /*
-        [TestMethod]
-        public void Read_OnlyForMe()
-        {
-            IRepository<Message> repository = new MemoryMessageRepository();
-            IRepository<User> userRepository = new MemoryUserRepository();
-            MessageService message = new MessageService(repository, userRepository);
-            message.PostMessage(TEST_USER1, TEST_TEXT1);
-            message.PostMessage(TEST_USER2, TEST_TEXT2);
-
-            var messages = message.Read(TEST_USER1);
-            Assert.AreEqual(messages.Count, 1);
-            Assert.AreEqual(messages[0].Text, TEST_TEXT1);
-        }
-
-        [TestMethod]
-        public void Read_Sorted()
-        {
-            IRepository<Message> repository = new MemoryMessageRepository();
-            IRepository<User> userRepository = new MemoryUserRepository();
-            MessageService message = new MessageService(repository, userRepository);
             DateTime now = DateTime.Now;
-            message.PostMessage(new User(TEST_USER1), TEST_TEXT1, now);
-            message.PostMessage(new User(TEST_USER1), TEST_TEXT2, now.AddMinutes(1));
+            var user1 = new User(TEST_USER1);
+            var message1 = new Message() { Text = TEST_TEXT1, Username = user1, Timespan = now};
+            var message2 = new Message() { Text = TEST_TEXT2, Username = user1, Timespan = now.AddMinutes(1) };
+            memoryUserRepository.Add(user1);
+            memoryMessageRepository.Add(message1);
+            memoryMessageRepository.Add(message2);
 
-            var messages = message.Read(TEST_USER1);
-            Assert.AreEqual(messages.Count, 2);
+            commandRead.Process(TEST_USER1);
+            var messages = commandRead.Output;
+
             Assert.AreEqual(messages[0].Text, TEST_TEXT2);
             Assert.AreEqual(messages[1].Text, TEST_TEXT1);
         }
-
-        [TestMethod]
-        public void Read_NoMessage()
-        {
-            IRepository<Message> repository = new MemoryMessageRepository();
-            IRepository<User> userRepository = new MemoryUserRepository();
-            MessageService message = new MessageService(repository, userRepository);
-
-            var messages = message.Read(TEST_USER1);
-            Assert.AreEqual(messages.Count, 0);
-        }
-        */
     }
 }
